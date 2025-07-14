092eb6d287a113ec5247c81a0c5773e99db0b757
hollis7 <herryhollis@163.com>
2025-07-14 15:24:30 +0800

    add ch6

---
+++ src/ch6/exercise6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+// 例子
+int count_add(int n) // n是形参
+{
+    static int ctr = 0; // ctr 是局部静态变量
+    ctr += n;
+    return ctr;
+}
+
+int main()
+{
+    for (int i = 0; i != 10; ++i) // i 是局部变量
+        cout << count_add(i)<<" ";
+
+    return 0;
+}
