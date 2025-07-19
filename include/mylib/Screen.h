476b7fad1900f0d3fcf92cd5a60358cb166563e7
hollis7 <herryhollis@163.com>
2025-07-19 11:07:14 +0800

    update screen_h

--- include/mylib/Screen.h
+++ include/mylib/Screen.h
@@ -1,8 +1,11 @@
-#ifndef SCREEN_H
-#define SCREEN_H
+// 前向声明
+class Window_mgr;
 
 class Screen
 {
+    // 声明Window_mgr为Screen的友元类
+    friend class Window_mgr;
+
 public:
     typedef std::string::size_type pos;
     Screen() = default; // 默认构造函数
@@ -26,6 +29,25 @@ private:
         os << contents; // 显示屏幕内容
     }
 };
+
+class Window_mgr
+{
+public:
+    using ScreenIndex = std::vector<Screen>::size_type; // 定义屏幕索引类型
+    void clear(ScreenIndex);                            // 清除指定屏幕的内容
+
+private:
+    std::vector<Screen> screens{Screen(10, 5, '#')}; // 存储屏幕对象的向量
+};
+
+// 现在可以声明Window_mgr::clear为友元函数
+inline void Window_mgr::clear(ScreenIndex i)
+{
+    Screen &s = screens[i];                            // 获取指定索引的屏幕
+    s.contents = std::string(s.height * s.width, ' '); // 清空屏幕内容
+    s.cursor = 0;                                      // 重置光标位置
+}
+
 inline char Screen::get(pos r, pos c) const
 {
     return contents[r * width + c]; // 返回指定位置的字符
@@ -47,24 +69,21 @@ inline Screen &Screen::set(char c)
     contents[cursor] = c; // 设置光标位置的字符
     return *this;         // 返回当前对象的引用
 }
+
 inline Screen &Screen::set(pos r, pos c, char ch)
 {
     contents[r * width + c] = ch; // 设置指定位置的字符
     return *this;                 // 返回当前对象的引用
 }
+
 Screen &Screen::display(std::ostream &os)
 {
     do_display(os); // 调用私有成员函数显示内容
     return *this;   // 返回当前对象的引用
 }
+
 const Screen &Screen::display(std::ostream &os) const
 {
     do_display(os); // 调用私有成员函数显示内容
     return *this;   // 返回当前对象的引用
 }
-class Window_mgr
-{
-private:
-    std::vector<Screen> screens{Screen(24, 80, ' ')}; // 存储屏幕对象的向量
-};
-#endif
--- src/ch7/exercise7_1.cpp
+++ src/ch7/exercise7_1.cpp
@@ -5,10 +5,8 @@
 #include "Screen.h"
 int main()
 {
-    Screen myScreen(5, 5, 'X'); // 创建一个5x5的屏幕，初始字符为'X'
-    const Screen blank(5, 3, 'c');
-    myScreen.set('#').display(std::cout); // 设置光标位置的字符为'#'并显示屏幕内容
-    std::cout << std::endl;
-    blank.display(std::cout); // 显示另一个屏幕内容
+    Window_mgr wm; // 创建一个Window_mgr对象
+    wm.clear(0);  // 清除第一个屏幕的内容
+
     return 0;
 }
